3be0201f9e9e3fcefdb173e3885f592d1c3d44dd
Adit Anand <adit.k.anand@gmail.com>
2025-01-03 21:23:42 -0800

    The leak detector can now save the audio file while running when a filename is provided. This saved file can also be re-analysed by the leak detector.

--- leak_detector.py
+++ leak_detector.py
@@ -16,15 +16,16 @@
 import argparse
 import time
 
+import numpy
+
 from mediapipe.tasks import python
 from mediapipe.tasks.python.audio.core import audio_record
 from mediapipe.tasks.python.components import containers
 from mediapipe.tasks.python import audio
 from utils import Plotter
 
-
 def run(model: str, max_results: int, score_threshold: float,
-        overlapping_factor: float) -> None:
+        overlapping_factor: float, a_file_name:str, upload_file_name:str, total_runtime:int) -> None:
   """Continuously run inference on audio data acquired from the device.
 
   Args:
@@ -33,6 +34,8 @@ def run(model: str, max_results: int, score_threshold: float,
     score_threshold: The score threshold of classification results.
     overlapping_factor: Target overlapping between adjacent inferences.
   """
+  if a_file_name != None :
+    a_file = open(a_file_name,'wb')
 
   if (overlapping_factor <= 0) or (overlapping_factor >= 1.0):
     raise ValueError('Overlapping factor must be between 0 and 1.')
@@ -47,6 +50,7 @@ def run(model: str, max_results: int, score_threshold: float,
   def save_result(result: audio.AudioClassifierResult, timestamp_ms: int):
     result.timestamp_ms = timestamp_ms
     classification_result_list.append(result)
+    #print(result)
 
   # Initialize the audio classification model.
   base_options = python.BaseOptions(model_asset_path=model)
@@ -63,7 +67,6 @@ def run(model: str, max_results: int, score_threshold: float,
   audio_format = containers.AudioDataFormat(num_channels, sample_rate)
   record = audio_record.AudioRecord(num_channels, sample_rate, buffer_size)
   audio_data = containers.AudioData(buffer_size, audio_format)
-
   # We'll try to run inference every interval_between_inference seconds.
   # This is usually half of the model's input length to create an overlapping
   # between incoming audio segments to improve classification accuracy.
@@ -74,63 +77,127 @@ def run(model: str, max_results: int, score_threshold: float,
   last_inference_time = time.time()
 
   # Start audio recording in the background.
-  record.start_recording()
+  if upload_file_name == None:
+    record.start_recording()
 
-  faucet_runtime=0
-  last_analysis_time=0
-  
-  # Loop until the user close the classification results plot.
-  while True:
-    # Wait until at least interval_between_inference seconds has passed since
-    # the last inference.
-    now = time.time()
-    diff = now - last_inference_time
-    if diff < interval_between_inference:
-      time.sleep(pause_time)
-      continue
-    last_inference_time = now
+    faucet_runtime=0
+    last_analysis_time=0
+    timer_start = time.time()
+    
+    # Loop until the total runtime has been fulfilled.
+    while (time.time()-timer_start) <= total_runtime:
+      # Wait until at least interval_between_inference seconds has passed since
+      # the last inference.
+      now = time.time()
+      diff = now - last_inference_time
+      if diff < interval_between_inference:
+        time.sleep(pause_time)
+        continue
+      last_inference_time = now
 
-    # Load the input audio from the AudioRecord instance and run classify.
-    data = record.read(buffer_size)
-    # audio_data.load_from_array(data.astype(np.float32))
-    audio_data.load_from_array(data)
-    classifier.classify_async(audio_data, round(last_inference_time * 1000))
+      # Load the input audio from the AudioRecord instance and run classify.
+      data = record.read(buffer_size)
+      if a_file_name != None :
+        #turn data a 124800 byte chunk and put it into the audio file
+        a_file.write(data.tobytes()) 
 
-    # # Plot the classification results.
-    if classification_result_list:
-      #print(classification_result_list)
-      plotter.plot(classification_result_list[-1])
+      audio_data.load_from_array(data)
+      classifier.classify_async(audio_data, round(last_inference_time * 1000))
+
+      # Plot the classification results.
+      if classification_result_list:
+        plotter.plot(classification_result_list[-1])
+        
+
+      if last_analysis_time==0:
+        last_analysis_time=now
+      diff = now - last_analysis_time
+      if diff >= 5:
+        faucet_score_sum=0
+        score_count=0
+        last_analysis_time = now
+        for i in range (len(classification_result_list)):
+          result=classification_result_list[i]
+          if i==0:
+            start_time=result.timestamp_ms
+          ts=result.timestamp_ms-start_time
+          score_count+=2
+          for classification in result.classifications:
+            for i in range(len(classification.categories)):
+              category=classification.categories[i]
+              if category.category_name == 'Water tap, faucet' or category.category_name == 'Sink (filling or washing)':
+                faucet_score_sum+=category.score
+        faucet_score_average=faucet_score_sum/score_count
+        if faucet_score_average>=0.5:
+          faucet_runtime+=5
+          print("The faucet has been running for",faucet_runtime,"seconds.")
+        else:
+          faucet_runtime=0
+        classification_result_list.clear()
+        if faucet_runtime==30:
+          print("ALERT: YOUR FAUCET HAS BEEN RUNNING FOR 10 MINUTES")
+  else:
+    faucet_runtime=0
+    last_analysis_time=0
+    timer_start = time.time()
       
+    # Loop until uploaded file has been read through
+    with open(upload_file_name, 'rb') as f:
+      while True:
+          chunk = f.read(124800)  # read 124800 bytes at a time
+          time.sleep(0.975) # delays time to simulate real time audio
+          if not chunk:
+            break
+          data = numpy.frombuffer(chunk, dtype = numpy.float64)
+          # Wait until at least interval_between_inference seconds has passed since
+          # the last inference.
+          now = time.time()
+          diff = now - last_inference_time
+          if diff < interval_between_inference:
+            time.sleep(pause_time)
+            continue
 
-    if last_analysis_time==0:
-      last_analysis_time=now
-    diff = now - last_analysis_time
-    if diff >= 5:
-      #print(classification_result_list,"code")
-      faucet_score_sum=0
-      score_count=0
-      last_analysis_time = now
-      for i in range (len(classification_result_list)):
-        result=classification_result_list[i]
-        if i==0:
-          start_time=result.timestamp_ms
-        ts=result.timestamp_ms-start_time
-        score_count+=2
-        for classification in result.classifications:
-          for i in range(len(classification.categories)):
-            category=classification.categories[i]
-            if category.category_name == 'Water tap, faucet' or category.category_name == 'Sink (filling or washing)':
-              faucet_score_sum+=category.score
-      faucet_score_average=faucet_score_sum/score_count
-      #print(faucet_score_sum,score_count,faucet_score_average)
-      if faucet_score_average>=0.5:
-        faucet_runtime+=5
-        print("The faucet has been running for",faucet_runtime,"seconds.")
-      else:
-        faucet_runtime=0
-      classification_result_list.clear()
-      if faucet_runtime==600:
-        print("ALERT: YOUR FAUCET HAS BEEN RUNNING FOR 10 MINUTES")
+          last_inference_time = now
+
+          # Load the input audio from the AudioRecord instance and run classify.
+
+          audio_data.load_from_array(data)
+          classifier.classify_async(audio_data, round(last_inference_time * 1000))
+
+          # Plot the classification results.
+          if classification_result_list:
+            plotter.plot(classification_result_list[-1])
+          
+
+          if last_analysis_time==0:
+            last_analysis_time=now
+          diff = now - last_analysis_time
+          if diff >= 5:
+            faucet_score_sum=0
+            score_count=0
+            last_analysis_time = now
+            for i in range (len(classification_result_list)):
+              result=classification_result_list[i]
+              if i==0:
+                start_time=result.timestamp_ms
+              ts=result.timestamp_ms-start_time
+              score_count+=2
+              for classification in result.classifications:
+                for i in range(len(classification.categories)):
+                  category=classification.categories[i]
+                  if category.category_name == 'Water tap, faucet' or category.category_name == 'Sink (filling or washing)':
+                    faucet_score_sum+=category.score
+            faucet_score_average=faucet_score_sum/score_count
+            if faucet_score_average>=0.5:
+              faucet_runtime+=5
+              print("The faucet has been running for",faucet_runtime,"seconds.")
+            else:
+              faucet_runtime=0
+            classification_result_list.clear()
+            if faucet_runtime==30:
+              print("ALERT: YOUR FAUCET HAS BEEN RUNNING FOR 10 MINUTES")
+
+  a_file.close()
     
     
 
@@ -157,11 +224,24 @@ def main():
       help='The score threshold of classification results.',
       required=False,
       default=0.0)
+  parser.add_argument(
+    '--audio_save_file_name',
+    help='The name of the file where audio will be saved.',
+    required=False,
+    default=None)
+  parser.add_argument(
+    '--runtime',
+    help='The amount of time the program will run in seconds',
+    required=True)
+  parser.add_argument(
+    '--upload_file_name',
+    help='Name of file with audio data',
+    required=False,
+    default = None)
   args = parser.parse_args()
 
   run(args.model, int(args.maxResults), float(args.scoreThreshold),
-      float(args.overlappingFactor))
-
+      float(args.overlappingFactor), str(args.audio_save_file_name), str(args.upload_file_name), int(args.runtime))
 
 if __name__ == '__main__':
   main()
